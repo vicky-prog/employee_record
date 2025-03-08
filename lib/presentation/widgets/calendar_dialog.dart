26f6b7ea0994c788c7e5ba3ad770250c71a5fec9
vignesh <johnvick281997@gmail.com>
2025-03-08 18:18:44 +0530

    renderflow bug fix

--- lib/presentation/widgets/calendar_dialog.dart
+++ lib/presentation/widgets/calendar_dialog.dart
@@ -44,191 +44,194 @@ class _CalendarDialogState extends State<CalendarDialog> {
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           child: ConstrainedBox(
                constraints: BoxConstraints(
-          maxWidth: 500, // Adjust width dynamically
+          maxWidth: 500,
+          maxHeight: constraints.maxHeight * 0.9
         ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // Quick Select Buttons
-                Padding(
-                  padding:  EdgeInsets.only(left: _defaultPadding, right: _defaultPadding, top: _defaultPadding),
-                  child: Column(
-                    children: [
-                      widget.toDate ?? false
-                          ? Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Row(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Quick Select Buttons
+                  Padding(
+                    padding:  EdgeInsets.only(left: _defaultPadding, right: _defaultPadding, top: _defaultPadding),
+                    child: Column(
+                      children: [
+                        widget.toDate ?? false
+                            ? Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Row(
+                                children: [
+                                  Expanded(child: _quickSelectButton("No Date", null)),
+                                  SizedBox(width: 10),
+                                  Expanded(
+                                    child: _quickSelectButton("Today", DateTime.now()),
+                                  ),
+                                ],
+                              ),
+                            )
+                            : Column(
                               children: [
-                                Expanded(child: _quickSelectButton("No Date", null)),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: _quickSelectButton("Today", DateTime.now()),
-                                ),
-                              ],
-                            ),
-                          )
-                          : Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: _quickSelectButton(
-                                      "Today",
-                                      DateTime.now(),
-                                    ),
-                                  ),
-                                  SizedBox(width: 10),
-                                  Expanded(
-                                    child: _quickSelectButton(
-                                      "Next Monday",
-                                      _getNextMonday(),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: _quickSelectButton(
-                                      "Next Tuesday",
-                                      _getNextTuesday(),
-                                    ),
-                                  ),
-                                  SizedBox(width: 10),
-                                  Expanded(
-                                    child: _quickSelectButton(
-                                      "After 1 Week",
-                    
-                                      DateTime.now().add(Duration(days: 7)),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                    ],
-                  ),
-                ),
-                    
-                SizedBox(height: 16),
-                // Calendar Widget
-                Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: _defaultPadding),
-                  child: TableCalendar(
-                              
-                    focusedDay: _selectedDay ?? DateTime.now(),
-                    firstDay: DateTime.utc(2020, 1, 1),
-                    lastDay: DateTime.utc(2030, 12, 31),
-                    calendarStyle: CalendarStyle(
-                      todayDecoration: BoxDecoration(
-                        color: Colors.blue.shade100,
-                        shape: BoxShape.circle,
-                      ),
-                      selectedDecoration: BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    // Disable past dates
-                    enabledDayPredicate: (day) {
-                      if(widget.allowPast!){
-                       return true;
-                      }
-                      var today = DateTime.now();
-                      if(widget.disableDay != null){
-                         today = widget.disableDay!;
-                      }
-                      return !day.isBefore(
-                        DateTime(today.year, today.month, today.day),
-                      ); // Allow today & future
-                    },
-                      
-                    headerStyle: HeaderStyle(
-                      formatButtonVisible: false,
-                      titleCentered: true,
-                      leftChevronMargin: EdgeInsets.only(left: 30),
-                      rightChevronMargin: EdgeInsets.only(right: 30),
-                      leftChevronIcon: Icon(Icons.chevron_left, color: Colors.black),
-                      rightChevronIcon: Icon(Icons.chevron_right, color: Colors.black),
-                    ),
-                    selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
-                    onDaySelected: (selectedDay, focusedDay) {
-                      
-                      setState(() => _selectedDay = selectedDay);
-                    },
-                  ),
-                ),
-                Divider(color: Colors.grey.shade300,thickness: 0.8,),
-                Padding(
-                  padding:  EdgeInsets.only(left: _defaultPadding,right: _defaultPadding,bottom: _defaultPadding),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: SizedBox(
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.calendar_today,
-                                color: Colors.blue,
-                                size: 20,
-                              ),
-                              SizedBox(width: 5),
-                              Text(
-                                _selectedDay == null
-                                    ? "No Date"
-                                    : DateFormat('d MMM yyyy').format(_selectedDay!),
-                                style: TextStyle(
-                                  fontSize: 15,
-                                 // fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    
-                      Expanded(
-                        child: SizedBox(
-                         // height: 45,
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
+                                Row(
                                   children: [
-                                    _actionButton(
-                                      "Cancel",
-                                      Colors.blue.shade50,
-                                      Colors.blue,
-                                      () => Navigator.pop(context),
+                                    Expanded(
+                                      child: _quickSelectButton(
+                                        "Today",
+                                        DateTime.now(),
+                                      ),
                                     ),
                                     SizedBox(width: 10),
-                                    _actionButton(
-                                      "Save",
-                                      Colors.blue,
-                                      Colors.white,
-                                      () {
-                                        if (_selectedDay != null) {
-                                          widget.onDateSelected(_selectedDay!);
-                                        } else {
-                                          widget.onDateSelected(null);
-                                        }
-                    
-                                        Navigator.pop(context);
-                                      },
+                                    Expanded(
+                                      child: _quickSelectButton(
+                                        "Next Monday",
+                                        _getNextMonday(),
+                                      ),
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
-                          ),
+                                SizedBox(height: 10),
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: _quickSelectButton(
+                                        "Next Tuesday",
+                                        _getNextTuesday(),
+                                      ),
+                                    ),
+                                    SizedBox(width: 10),
+                                    Expanded(
+                                      child: _quickSelectButton(
+                                        "After 1 Week",
+                      
+                                        DateTime.now().add(Duration(days: 7)),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                      ],
+                    ),
+                  ),
+                      
+                  SizedBox(height: 16),
+                  // Calendar Widget
+                  Padding(
+                    padding:  EdgeInsets.symmetric(horizontal: _defaultPadding),
+                    child: TableCalendar(
+                                
+                      focusedDay: _selectedDay ?? DateTime.now(),
+                      firstDay: DateTime.utc(2020, 1, 1),
+                      lastDay: DateTime.utc(2030, 12, 31),
+                      calendarStyle: CalendarStyle(
+                        todayDecoration: BoxDecoration(
+                          color: Colors.blue.shade100,
+                          shape: BoxShape.circle,
+                        ),
+                        selectedDecoration: BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
                         ),
                       ),
-                    ],
+                      // Disable past dates
+                      enabledDayPredicate: (day) {
+                        if(widget.allowPast!){
+                         return true;
+                        }
+                        var today = DateTime.now();
+                        if(widget.disableDay != null){
+                           today = widget.disableDay!;
+                        }
+                        return !day.isBefore(
+                          DateTime(today.year, today.month, today.day),
+                        ); // Allow today & future
+                      },
+                        
+                      headerStyle: HeaderStyle(
+                        formatButtonVisible: false,
+                        titleCentered: true,
+                        leftChevronMargin: EdgeInsets.only(left: 30),
+                        rightChevronMargin: EdgeInsets.only(right: 30),
+                        leftChevronIcon: Icon(Icons.chevron_left, color: Colors.black),
+                        rightChevronIcon: Icon(Icons.chevron_right, color: Colors.black),
+                      ),
+                      selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+                      onDaySelected: (selectedDay, focusedDay) {
+                        
+                        setState(() => _selectedDay = selectedDay);
+                      },
+                    ),
                   ),
-                ),
-              ],
+                  Divider(color: Colors.grey.shade300,thickness: 0.8,),
+                  Padding(
+                    padding:  EdgeInsets.only(left: _defaultPadding,right: _defaultPadding,bottom: _defaultPadding),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.calendar_today,
+                                  color: Colors.blue,
+                                  size: 20,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  _selectedDay == null
+                                      ? "No Date"
+                                      : DateFormat('d MMM yyyy').format(_selectedDay!),
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                   // fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      
+                        Expanded(
+                          child: SizedBox(
+                           // height: 45,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      _actionButton(
+                                        "Cancel",
+                                        Colors.blue.shade50,
+                                        Colors.blue,
+                                        () => Navigator.pop(context),
+                                      ),
+                                      SizedBox(width: 10),
+                                      _actionButton(
+                                        "Save",
+                                        Colors.blue,
+                                        Colors.white,
+                                        () {
+                                          if (_selectedDay != null) {
+                                            widget.onDateSelected(_selectedDay!);
+                                          } else {
+                                            widget.onDateSelected(null);
+                                          }
+                      
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
